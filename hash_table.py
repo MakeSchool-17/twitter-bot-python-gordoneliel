375c30406173d7480cfd355be56080d0753a47a2
gordoneliel <gordoneliel@gmail.com>
2015-09-26 12:11:27 -0700

    Added a feature branch to test hash_table with tuples

--- hash_table.py
+++ hash_table.py
@@ -9,10 +9,8 @@ class HashTable:
 
     ''' set a key value pair in to the HashTable  or replace existing '''
     def set(self, key, value):
-        key_str = hash(key)
-        print(key_str)
-        bucket = self.hash(key_str, len(self.buckets))
-
+        bucket = self.base_hash(key, len(self.buckets))
+        print(key, value, bucket)
         if self.buckets[bucket] is None:
             self.buckets[bucket] = key
             self.value[bucket] = value
@@ -20,9 +18,10 @@ class HashTable:
             if self.buckets[bucket] == key:
                 self.value[bucket] = value
             else:
-                next_bucket = double_hash(key_str, len(self.buckets), bucket)
+                next_bucket = self.double_hash(key, len(self.buckets), bucket)
+                print(next_bucket)
                 while self.buckets[next_bucket] is not None and self.buckets[next_bucket] is not key:
-                    next_bucket = double_hash(key_str, len(self.buckets),
+                    next_bucket = self.double_hash(key, len(self.buckets),
                     next_bucket)
 
                     if self.buckets[next_bucket] is not None:
@@ -33,8 +32,7 @@ class HashTable:
 
     ''' Get a key value pair from the HashTable '''
     def get(self, key):
-        key_str = hash(key)
-        initial_bucket = self.hash(key_str, len(self.buckets))
+        initial_bucket = self.base_hash(key, len(self.buckets))
         next_bucket = 0
 
         found = False
@@ -45,17 +43,21 @@ class HashTable:
                 found = True
                 return self.value[next_bucket]
             else:
-                next_bucket = double_hash(key_str, len(self.buckets), initial_bucket)
+                next_bucket = self.double_hash(key, len(self.buckets), initial_bucket)
 
         return self.value[initial_bucket] if found else None
 
     ''' A hash function for hashing the key '''
-    def hash(self, key, size):
-        return key % size
+    def base_hash(self, key, size):
+        hashValue = 0
+        for char in key:
+            hashValue += ord(char)
+
+        return hashValue % size
 
     ''' Second hash function for double hashing '''
     def double_hash(self, key, size, old_bucket):
-        return old_bucket * hash(self, key, size)
+        return old_bucket * self.base_hash(key, size)
 
     ''' Setters and Getters '''
     def __setitem__(self, key, value):
@@ -80,16 +82,16 @@ class HashTable:
         # table.set(1, 8)
         table.set("Bitonic", 5)
         table.set("Lambda", 90)
-        # table.set(1, "one")
-        # table.set(4, "joe")
-        # table.set(3, "peter")
-        # table[3] = "Nancy"
-        # table[11] = "Fiona"
+        table.set("Joey", 90)
+        table.set("34", 78)
+        table.set("Molly", 90)
+        table.set("654", 18)
+        # table.set("Bi", 6)
         print(table.value)
         print(table.buckets)
         print(len(table))
         print(table)
-        print(table.get("Lambda"))
+        # print(table.get("Lambda"))
 
 if __name__ == '__main__':
     HashTable.main()
