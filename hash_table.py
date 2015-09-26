7e1b15bee7d45d9d1269205e46701c4341c0a232
gordoneliel <gordoneliel@gmail.com>
2015-09-26 11:05:50 -0700

    HashTable get works partially, incomplete implementation

--- hash_table.py
+++ hash_table.py
@@ -9,7 +9,9 @@ class HashTable:
 
     ''' set a key value pair in to the HashTable  or replace existing '''
     def set(self, key, value):
-        bucket = self.hash(key, len(self.buckets))
+        key_str = hash(key)
+        print(key_str)
+        bucket = self.hash(key_str, len(self.buckets))
 
         if self.buckets[bucket] is None:
             self.buckets[bucket] = key
@@ -18,9 +20,9 @@ class HashTable:
             if self.buckets[bucket] == key:
                 self.value[bucket] = value
             else:
-                next_bucket = double_hash(key, len(self.buckets), bucket)
+                next_bucket = double_hash(key_str, len(self.buckets), bucket)
                 while self.buckets[next_bucket] is not None and self.buckets[next_bucket] is not key:
-                    next_bucket = double_hash(key, len(self.buckets),
+                    next_bucket = double_hash(key_str, len(self.buckets),
                     next_bucket)
 
                     if self.buckets[next_bucket] is not None:
@@ -30,16 +32,22 @@ class HashTable:
                         self.value[next_bucket] = value
 
     ''' Get a key value pair from the HashTable '''
-    def get(self, key, value):
-        initial_bucket = self.hash(key, len(self.buckets))
+    def get(self, key):
+        key_str = hash(key)
+        initial_bucket = self.hash(key_str, len(self.buckets))
+        next_bucket = 0
 
         found = False
 
-        while self.buckets[initial_bucket] is not None and not found:
-            if self.value[initial_bucket] == value:
+        while self.buckets[initial_bucket] is not None and not found and next_bucket != initial_bucket:
+            next_bucket = initial_bucket
+            if self.buckets[next_bucket] == key:
                 found = True
+                return self.value[next_bucket]
+            else:
+                next_bucket = double_hash(key_str, len(self.buckets), initial_bucket)
 
-        return self.buckets[key]
+        return self.value[initial_bucket] if found else None
 
     ''' A hash function for hashing the key '''
     def hash(self, key, size):
@@ -69,17 +77,19 @@ class HashTable:
     def main():
         # value = int(inset("Enter a value"))
         table = HashTable()
-        table.set(1, 8)
-        table.set(1, "one")
-        table.set(4, "joe")
-        table.set(3, "peter")
-        table[3] = "Nancy"
-        table[11] = "Fiona"
-        # print(table.value)
-        # print(table.buckets)
+        # table.set(1, 8)
+        table.set("Bitonic", 5)
+        table.set("Lambda", 90)
+        # table.set(1, "one")
+        # table.set(4, "joe")
+        # table.set(3, "peter")
+        # table[3] = "Nancy"
+        # table[11] = "Fiona"
+        print(table.value)
+        print(table.buckets)
         print(len(table))
         print(table)
-        print(table.get(4, "joe"))
+        print(table.get("Lambda"))
 
 if __name__ == '__main__':
     HashTable.main()
